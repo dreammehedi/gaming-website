72cf48d548dd86415cabaa3f3d6c5b2625c15316
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-01-18 20:39:01 +0600

    custom fonts add |Barlow, Poppins, Fira-Sans|

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,12 @@ module.exports = {
   content: ["./src/**/*.{html, js}"],
   theme: {
     extend: {
+      fontFamily:{
+        poppins: ['Poppins', 'sans-serif'],
+        'fira-sans': ['Fira Sans', 'sans-serif'],
+        barlow: ['Barlow', 'sans-serif']
+      }
     },
   },
   plugins: [],
 }
-
